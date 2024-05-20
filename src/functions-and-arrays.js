c8f7cde4ffe4044f3ac49ad9579146ee47d4e471
Pedro Cuzzolin <pedrocuzzo@gmail.com>
2024-05-20 14:12:16 +0200

    solvedLab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,25 +1,61 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else { 
+    return num2;
+  }
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+
+function findLongestWord(words) {
+  if (words.length === 0) { 
+    return null;
+  }  
+  let longestWord = words[0]
+  for (let i = 1; i < words.lenght; i++){
+    if (words[1].lenght > longestWord.lenght) {
+      longestWord = words[i];
+    }
+  }
+  return longestWord
+}
+
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+
+function sumNumbers(numbers) {
+  if (numbers.lenght === 0) {
+    return 0;
+  } 
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+  }
+  return sum;
+}
+
 
 
 
 // Iteration #3.1 Bonus:
 function sum() {}
+const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10];
 
+function sum(mixedArr)  {
+
+ 
+}
 
 
 // Iteration #4: Calculate the average
